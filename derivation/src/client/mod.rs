e733fe8853e61fe30a436ac54351ca627d1c3882
yoshidan <naohiro.y@gmail.com>
2025-02-10 22:26:47 +0900

    rerun evm on elc (can't use dynamic oracle in revm )

--- derivation/src/client/mod.rs
+++ derivation/src/client/mod.rs
@@ -1,18 +1,1 @@
-use kona_preimage::{HintWriter, OracleReader};
-use kona_std_fpvm::{FileChannel, FileDescriptor};
-
 pub mod precompiles;
-
-//TODO It may be invalid.
-static ORACLE_READER_PIPE: FileChannel =
-    FileChannel::new(FileDescriptor::PreimageRead, FileDescriptor::PreimageWrite);
-
-/// The global hint writer pipe.
-static HINT_WRITER_PIPE: FileChannel =
-    FileChannel::new(FileDescriptor::HintRead, FileDescriptor::HintWrite);
-
-/// The global preimage oracle reader.
-static ORACLE_READER: OracleReader<FileChannel> = OracleReader::new(ORACLE_READER_PIPE);
-
-/// The global hint writer.
-static HINT_WRITER: HintWriter<FileChannel> = HintWriter::new(HINT_WRITER_PIPE);
