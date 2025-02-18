209ae3e69d1562c53b2074e33ed112d10f9576b4
yoshidan <naohiro.y@gmail.com>
2025-02-18 15:52:48 +0900

    remove unused

--- elc/light-client/src/lib.rs
+++ elc/light-client/src/lib.rs
@@ -1,12 +1,27 @@
 #![no_std]
 extern crate alloc;
 
-mod client;
-mod client_state;
-mod consensus_state;
-mod errors;
-mod header;
+use alloc::string::ToString;
+pub use ethereum_ibc as l1_ibc;
+
+pub mod client;
+pub mod client_state;
+pub mod consensus_state;
+pub mod errors;
+pub mod header;
 mod l1;
 mod message;
 mod misc;
 mod oracle;
+
+
+pub fn register_implementations<const SYNC_COMMITTEE_SIZE: usize>(
+    registry: &mut dyn light_client::LightClientRegistry,
+) {
+    registry
+        .put_light_client(
+            client_state::OPTIMISM_CLIENT_STATE_TYPE_URL.to_string(),
+            alloc::boxed::Box::new(client::OptimismLightClient::<SYNC_COMMITTEE_SIZE>),
+        )
+        .unwrap()
+}
