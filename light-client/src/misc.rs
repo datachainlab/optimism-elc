2e24cbdbed5484ab4d0256ca2ba19b3834b2c24b
yoshidan <naohiro.y@gmail.com>
2025-04-21 17:11:04 +0900

    add test

--- light-client/src/misc.rs
+++ light-client/src/misc.rs
@@ -37,3 +37,141 @@ pub fn validate_header_timestamp_not_future(
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::errors::Error;
+    use crate::misc::{
+        validate_header_timestamp_not_future, validate_state_timestamp_within_trusting_period,
+    };
+    use core::time::Duration;
+    use light_client::types::Time;
+    use time::macros::datetime;
+    use time::OffsetDateTime;
+
+    #[test]
+    fn test_trusting_period_validation() {
+        {
+            let current_timestamp = datetime!(2023-08-20 0:00 UTC);
+            let trusted_state_timestamp = datetime!(2023-08-20 0:00 UTC);
+            validate_and_assert_trusting_period_no_error(
+                current_timestamp,
+                1,
+                trusted_state_timestamp,
+            );
+        }
+
+        // trusting_period
+        {
+            let current_timestamp = datetime!(2023-08-20 0:00 UTC);
+            let untrusted_header_timestamp = current_timestamp - Duration::new(0, 1);
+            let trusted_state_timestamp = untrusted_header_timestamp - Duration::new(0, 1);
+            validate_and_assert_trusting_period_error(
+                current_timestamp,
+                1,
+                trusted_state_timestamp,
+            );
+            validate_and_assert_trusting_period_error(
+                current_timestamp,
+                2,
+                trusted_state_timestamp,
+            );
+            validate_and_assert_trusting_period_no_error(
+                current_timestamp,
+                3,
+                trusted_state_timestamp,
+            );
+        }
+
+        // clock drift
+        {
+            let current_timestamp = datetime!(2023-08-20 0:00 UTC);
+            let untrusted_header_timestamp = current_timestamp + Duration::new(0, 1);
+            validate_and_assert_clock_drift_error(current_timestamp, 0, untrusted_header_timestamp);
+            validate_and_assert_clock_drift_error(current_timestamp, 1, untrusted_header_timestamp);
+            validate_and_assert_clock_drift_no_error(
+                current_timestamp,
+                2,
+                untrusted_header_timestamp,
+            );
+        }
+    }
+
+    fn validate_and_assert_trusting_period_no_error(
+        current_timestamp: OffsetDateTime,
+        trusting_period: u64,
+        trusted_state_timestamp: OffsetDateTime,
+    ) {
+        let result = validate_state_timestamp_within_trusting_period(
+            Time::from_unix_timestamp_nanos(current_timestamp.unix_timestamp_nanos() as u128)
+                .unwrap(),
+            Duration::from_nanos(trusting_period),
+            Time::from_unix_timestamp_nanos(trusted_state_timestamp.unix_timestamp_nanos() as u128)
+                .unwrap(),
+        );
+        assert!(result.is_ok());
+    }
+
+    fn validate_and_assert_trusting_period_error(
+        current_timestamp: OffsetDateTime,
+        trusting_period: u64,
+        trusted_state_timestamp: OffsetDateTime,
+    ) {
+        let result = validate_state_timestamp_within_trusting_period(
+            Time::from_unix_timestamp_nanos(current_timestamp.unix_timestamp_nanos() as u128)
+                .unwrap(),
+            Duration::from_nanos(trusting_period),
+            Time::from_unix_timestamp_nanos(trusted_state_timestamp.unix_timestamp_nanos() as u128)
+                .unwrap(),
+        );
+        if let Err(e) = result {
+            match e {
+                Error::OutOfTrustingPeriod(_, _) => {}
+                _ => panic!("unexpected error: {e}"),
+            }
+        } else {
+            panic!("expected error");
+        }
+    }
+
+    fn validate_and_assert_clock_drift_no_error(
+        current_timestamp: OffsetDateTime,
+        clock_drift: u64,
+        untrusted_header_timestamp: OffsetDateTime,
+    ) {
+        let result = validate_header_timestamp_not_future(
+            Time::from_unix_timestamp_nanos(current_timestamp.unix_timestamp_nanos() as u128)
+                .unwrap(),
+            Duration::from_nanos(clock_drift),
+            Time::from_unix_timestamp_nanos(
+                untrusted_header_timestamp.unix_timestamp_nanos() as u128
+            )
+            .unwrap(),
+        );
+        assert!(result.is_ok());
+    }
+
+    fn validate_and_assert_clock_drift_error(
+        current_timestamp: OffsetDateTime,
+        clock_drift: u64,
+        untrusted_header_timestamp: OffsetDateTime,
+    ) {
+        let result = validate_header_timestamp_not_future(
+            Time::from_unix_timestamp_nanos(current_timestamp.unix_timestamp_nanos() as u128)
+                .unwrap(),
+            Duration::from_nanos(clock_drift),
+            Time::from_unix_timestamp_nanos(
+                untrusted_header_timestamp.unix_timestamp_nanos() as u128
+            )
+            .unwrap(),
+        );
+        if let Err(e) = result {
+            match e {
+                Error::HeaderFromFuture(_, _, _) => {}
+                _ => panic!("unexpected error: {e}"),
+            }
+        } else {
+            panic!("expected error");
+        }
+    }
+}
