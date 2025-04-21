a60234e16f9150834f086ecf7a7f0984cfbd7846
yoshidan <naohiro.y@gmail.com>
2025-04-21 17:51:20 +0900

    add test

--- light-client/src/client.rs
+++ light-client/src/client.rs
@@ -266,3 +266,208 @@ fn gen_state_id(
         .map_err(LightClientError::commitment)
         .map_err(Error::LCPError)
 }
+
+#[cfg(test)]
+mod test {
+    use crate::client::OptimismLightClient;
+    use crate::client_state::ClientState;
+    use crate::consensus_state::ConsensusState;
+    use crate::l1::L1Config;
+    use alloc::collections::BTreeMap;
+    use alloc::string::{String, ToString};
+    use alloc::vec::Vec;
+    use alloy_primitives::hex;
+    use core::str::FromStr;
+    use ethereum_consensus::types::{Address, H256};
+    use light_client::commitments::CommitmentPrefix;
+    use light_client::types::{Any, ClientId, Height, Time};
+    use light_client::{ClientReader, HostClientReader, HostContext, LightClient};
+    use optimism_ibc_proto::ibc::lightclients::optimism::v1::L1Config as RawL1Config;
+    use prost::Message;
+
+    struct MockClientReader {
+        client_state: Option<ClientState>,
+        consensus_state: BTreeMap<Height, ConsensusState>,
+    }
+
+    impl HostContext for MockClientReader {
+        fn host_timestamp(&self) -> Time {
+            Time::now()
+        }
+    }
+
+    impl HostClientReader for MockClientReader {}
+
+    impl store::KVStore for MockClientReader {
+        fn set(&mut self, _key: Vec<u8>, _value: Vec<u8>) {
+            todo!()
+        }
+
+        fn get(&self, _key: &[u8]) -> Option<Vec<u8>> {
+            todo!()
+        }
+
+        fn remove(&mut self, _key: &[u8]) {
+            todo!()
+        }
+    }
+
+    impl ClientReader for MockClientReader {
+        fn client_state(&self, client_id: &ClientId) -> Result<Any, light_client::Error> {
+            let cs = self
+                .client_state
+                .clone()
+                .ok_or_else(|| light_client::Error::client_state_not_found(client_id.clone()))?;
+            Ok(Any::try_from(cs).unwrap())
+        }
+
+        fn consensus_state(
+            &self,
+            client_id: &ClientId,
+            height: &Height,
+        ) -> Result<Any, light_client::Error> {
+            let state = self
+                .consensus_state
+                .get(height)
+                .ok_or_else(|| {
+                    light_client::Error::consensus_state_not_found(client_id.clone(), *height)
+                })?
+                .clone();
+            Ok(Any::try_from(state).unwrap())
+        }
+    }
+
+    impl Default for ClientState {
+        fn default() -> Self {
+            let raw_l1_config = hex!("0a20acac7566fdf384a1ada45c01dcf9030d7eb0e1e5f5302659101d0b2a5bb590921001188edaa3bc06225e0a0400000001120e0a04010000011a0608691036183712160a04020000011a0e086910361837201928123016381c12160a04030000011a0e086910361837201928123016381c12160a04040000011a0e086910361837201928223026382c280630083808420408021003").to_vec();
+            let raw_l1_config = RawL1Config::decode(&*raw_l1_config).unwrap();
+            let l1_config = L1Config::try_from(raw_l1_config).unwrap();
+
+            ClientState {
+                chain_id: 0,
+                ibc_store_address: Default::default(),
+                ibc_commitments_slot: Default::default(),
+                trusting_period: Default::default(),
+                max_clock_drift: Default::default(),
+                latest_height: Default::default(),
+                frozen: false,
+                rollup_config: Default::default(),
+                l1_config,
+            }
+        }
+    }
+
+    impl Default for ConsensusState {
+        fn default() -> Self {
+            ConsensusState {
+                storage_root: Default::default(),
+                timestamp: Time::now(),
+                output_root: Default::default(),
+                l1_slot: Default::default(),
+                l1_current_sync_committee: Default::default(),
+                l1_next_sync_committee: Default::default(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_verify_membership() {
+        let (path, proof, value) = get_membership_proof();
+        let proof_height = Height::new(0, 1);
+        let client = OptimismLightClient::<
+            { ethereum_consensus::preset::minimal::PRESET.SYNC_COMMITTEE_SIZE },
+        >;
+
+        let mut cons_states = BTreeMap::new();
+        cons_states.insert(
+            proof_height.clone(),
+            ConsensusState {
+                storage_root: H256(hex!(
+                    "27cd08827e6bf1e435832f4b2660107beb562314287b3fa534f3b189574c0cca"
+                )),
+                ..Default::default()
+            },
+        );
+
+        let ctx = MockClientReader {
+            client_state: Some(ClientState {
+                ibc_store_address: Address(hex!("a7f733a4fEA1071f58114b203F57444969b86524")),
+                ibc_commitments_slot: H256(hex!(
+                    "1ee222554989dda120e26ecacf756fe1235cd8d726706b57517715dde4f0c900"
+                )),
+                latest_height: proof_height.clone(),
+                ..Default::default()
+            }),
+            consensus_state: cons_states,
+        };
+
+        let res = client.verify_membership(
+            &ctx,
+            ClientId::from_str("optimism-1").unwrap(),
+            CommitmentPrefix::new(),
+            path,
+            value,
+            proof_height,
+            proof,
+        );
+        assert!(res.is_ok(), "{:?}", res);
+    }
+
+    #[test]
+    fn test_verify_non_membership() {
+        let (path, proof) = get_non_membership_proof();
+        let proof_height = Height::new(0, 1);
+        let client = OptimismLightClient::<
+            { ethereum_consensus::preset::minimal::PRESET.SYNC_COMMITTEE_SIZE },
+        >;
+
+        let mut cons_states = BTreeMap::new();
+        cons_states.insert(
+            proof_height.clone(),
+            ConsensusState {
+                storage_root: H256(hex!(
+                    "27cd08827e6bf1e435832f4b2660107beb562314287b3fa534f3b189574c0cca"
+                )),
+                ..Default::default()
+            },
+        );
+
+        let ctx = MockClientReader {
+            client_state: Some(ClientState {
+                ibc_store_address: Address(hex!("a7f733a4fEA1071f58114b203F57444969b86524")),
+                ibc_commitments_slot: H256(hex!(
+                    "1ee222554989dda120e26ecacf756fe1235cd8d726706b57517715dde4f0c900"
+                )),
+                latest_height: proof_height.clone(),
+                ..Default::default()
+            }),
+            consensus_state: cons_states,
+        };
+        let res = client.verify_non_membership(
+            &ctx,
+            ClientId::from_str("optimism-1").unwrap(),
+            CommitmentPrefix::new(),
+            path,
+            proof_height,
+            proof,
+        );
+        assert!(res.is_ok(), "{:?}", res);
+    }
+
+    // returns: (path, proof, value)
+    fn get_membership_proof() -> (String, Vec<u8>, Vec<u8>) {
+        (
+            "clients/lcp-client-0/clientState".to_string(),
+            hex!("f90159f901118080a0143145e818eeff83817419a6632ea193fd1acaa4f791eb17282f623f38117f56a0e6ee0a993a7254ee9253d766ea005aec74eb1e11656961f0fb11323f4f91075580808080a01efae04adc2e970b4af3517581f41ce2ba4ff60492d33696c1e2a5ab70cb55bba03bac3f5124774e41fb6efdd7219530846f9f6441045c4666d2855c6598cfca00a020d7122ffc86cb37228940b5a9441e9fd272a3450245c9130ca3ab00bc1cd6ef80a0047f255205a0f2b0e7d29d490abf02bfb62c3ed201c338bc7f0088fa9c5d77eda069fecc766fcb2df04eb3a834b1f4ba134df2be114479e251d9cc9b6ba493077b80a094c3ed6a7ef63a6a67e46cc9876b9b1882eeba3d28e6d61bb15cdfb207d077e180f843a03e077f3dfd0489e70c68282ced0126c62fcef50acdcb7f57aa4552b87b456b11a1a05dc044e92e82db28c96fd98edd502949612b06e8da6dd74664a43a5ed857b298").to_vec(),
+            hex!("0a242f6962632e6c69676874636c69656e74732e6c63702e76312e436c69656e74537461746512ed010a208083673c69fe3f098ea79a799d9dbb99c39b4b4f17a1a79ef58bdf8ae86299951080f524220310fb012a1353575f48415244454e494e475f4e45454445442a1147524f55505f4f55545f4f465f44415445320e494e54454c2d53412d3030323139320e494e54454c2d53412d3030323839320e494e54454c2d53412d3030333334320e494e54454c2d53412d3030343737320e494e54454c2d53412d3030363134320e494e54454c2d53412d3030363135320e494e54454c2d53412d3030363137320e494e54454c2d53412d30303832383a14cb96f8d6c2d543102184d679d7829b39434e4eec48015001").to_vec()
+        )
+    }
+
+    // returns: (path, proof)
+    fn get_non_membership_proof() -> (String, Vec<u8>) {
+        (
+            "clients/lcp-client-1/clientState".to_string(),
+            hex!("f90114f901118080a0143145e818eeff83817419a6632ea193fd1acaa4f791eb17282f623f38117f56a0e6ee0a993a7254ee9253d766ea005aec74eb1e11656961f0fb11323f4f91075580808080a01efae04adc2e970b4af3517581f41ce2ba4ff60492d33696c1e2a5ab70cb55bba03bac3f5124774e41fb6efdd7219530846f9f6441045c4666d2855c6598cfca00a020d7122ffc86cb37228940b5a9441e9fd272a3450245c9130ca3ab00bc1cd6ef80a0047f255205a0f2b0e7d29d490abf02bfb62c3ed201c338bc7f0088fa9c5d77eda069fecc766fcb2df04eb3a834b1f4ba134df2be114479e251d9cc9b6ba493077b80a094c3ed6a7ef63a6a67e46cc9876b9b1882eeba3d28e6d61bb15cdfb207d077e180").to_vec()
+        )
+    }
+}
